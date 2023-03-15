61b81c76c1bb8af6d1815bb7b607f1c2e579806d
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-15 21:21:33 +0000

    Set up dummy controllers for User model

---
+++ members-only-app/controllers/userControllers.js
@@ -0,0 +1,17 @@
+const User = require("../models/user");
+
+exports.userCreateGet = (req, res, next) => {
+    res.send("TODO: implement userCreateGet")
+};
+
+exports.userCreatePost = (req, res, next) => {
+    res.send("TODO: implement userCreatePost")
+};
+
+exports.userStatusMemberGet = (req, res, next) => {
+    res.send("TODO: implement userStatusMemberGet")
+};
+
+exports.userStatusMemberPost = (req, res, next) => {
+    res.send("TODO: implement userStatusMemberPost");
+};
--- members-only-app/routes/membersOnly.js
+++ members-only-app/routes/membersOnly.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const indexGet = require("../controllers/indexGet");
+const userController = require("../controllers/userControllers");
 
 router.get("/", indexGet);
+
+router.get("/sign-up", userController.userCreateGet);
+
+router.post("/sign-up", userController.userCreatePost);
+
+router.get("/become-a-member", userController.userStatusMemberGet);
+
+router.post("/become-a-member", userController.userStatusMemberPost);
 
 module.exports = router;
