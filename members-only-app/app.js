13e89d1b6fc4967fa0fb8ff514062b877820b4a1
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-15 20:00:28 +0000

    Set up connection to development database

--- members-only-app/app.js
+++ members-only-app/app.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING);
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
