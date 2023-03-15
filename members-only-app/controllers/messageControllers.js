6ebf28b222ba70e3e51804ccfe8f8f0d5950f09a
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-15 21:37:14 +0000

    Set up dummy controllers for Message model

---
+++ members-only-app/controllers/messageControllers.js
@@ -0,0 +1,17 @@
+const Message = require("../models/message");
+
+exports.messageListGet = (req, res, next) => {
+    res.send("TODO: implement messageListGet");
+};
+
+exports.messageCreateGet = (req, res, next) => {
+    res.send("TODO: implement messageCreateGet");
+};
+
+exports.messageCreatePost = (req, res, next) => {
+    res.send("TODO: implement messageCreatePost");
+};
+
+exports.messageDeletePost = (req, res, next) => {
+    res.send("TODO: implement messageDeletePost");
+};
--- members-only-app/routes/membersOnly.js
+++ members-only-app/routes/membersOnly.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 const indexGet = require("../controllers/indexGet");
 const userController = require("../controllers/userControllers");
+const messageController = require("../controllers/messageControllers");
 
-router.get("/", indexGet);
+router.get("/", messageController.messageListGet);
 
 router.get("/sign-up", userController.userCreateGet);
-
 router.post("/sign-up", userController.userCreatePost);
 
 router.get("/become-a-member", userController.userStatusMemberGet);
-
 router.post("/become-a-member", userController.userStatusMemberPost);
+
+router.get("/create-message", messageController.messageCreateGet);
+router.post("/create-message", messageController.messageCreatePost);
+
+router.post("/delete-message", messageController.messageDeletePost);
 
 module.exports = router;
