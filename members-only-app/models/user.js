9f6fc7416a8341bbaa613695837221c14619e167
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-15 20:15:24 +0000

    Write UserSchema model

---
+++ members-only-app/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    forename: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["basic", "member", "admin"],
+        default: "basic",
+        required: true,
+    },
+});
+
+UserSchema.virtual("canSeeAuthorshipDetails").get(() => {
+    return this.status === "member" || this.status === "admin";
+});
+
+UserSchema.virtual("fullName").get(() => {
+    return `${this.forename} ${this.lastName}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
