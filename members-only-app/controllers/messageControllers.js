f1d46cf6b0845cfeda89d3100057f4ff6e3259cf
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-20 18:17:27 +0000

    Write messageCreatePost controller

--- members-only-app/controllers/messageControllers.js
+++ members-only-app/controllers/messageControllers.js
@@ -1,12 +1,37 @@
 const Message = require("../models/message");
+const { body, validationResult } = require("express-validator");
 
 exports.messageCreateGet = (req, res, next) => {
     res.render("messageForm")
 };
 
-exports.messageCreatePost = (req, res, next) => {
-    res.send("TODO: implement messageCreatePost");
-};
+exports.messageCreatePost = [
+    body("title")
+        .isLength({ min: 1 })
+        .withMessage("Please enter a title")
+        .escape(),
+    body("messageBody")
+        .isLength({ min: 1 })
+        .withMessage("Please enter a message body")
+        .escape(),
+    (req, res, next) => {
+        const errorResultObject = validationResult(req);
+        if (!errorResultObject.isEmpty()) {
+            return res.render("messageForm", {
+                persistedRequestBody: req.body,
+                errorArray: errorResultObject.array(),
+            });
+        }
+        const newMessage = new Message({
+            title: req.body.title,
+            messageBody: req.body.messageBody,
+            author: res.locals.currentUser.id,
+        });
+        newMessage.save()
+        .then(() => res.redirect("/"))
+        .catch((err) => next(err));
+    }
+];
 
 exports.messageDeletePost = (req, res, next) => {
     res.send("TODO: implement messageDeletePost");
